b4f3efcc1d216f1dd812793ffa12d72042c8f8fb
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-07-06 07:38:48 +0600

    Update quiz.dart

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -3,47 +3,62 @@ import 'package:quiz_app/start_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:quiz_app/question_screen.dart';
 
-class Quiz extends StatefulWidget
-{
+class Quiz extends StatefulWidget {
   const Quiz({super.key});
-  State<Quiz> createState(){
+
+  State<Quiz> createState() {
     return _QuizState();
   }
-
 }
-class _QuizState extends State<Quiz>
-{
-  Widget? activeScreen;  // ? This is mean active screen initial is null and we can use for update
-  // intisate class to switch screen on my app
 
+class _QuizState extends State<Quiz> {
+//***** Another Screen shif code with initstate Fucntion ****
+  // Widget? activeScreen;
 
-    @override
-  void initState() {
-      activeScreen =  Start_Screen(switchScreen);
-    super.initState();
-  }
- void switchScreen()
-  {
+  // ? This is mean active screen initial is null and we can use for update
+
+  //   @override
+  // void initState() {
+  //     activeScreen =  Start_Screen(switchScreen);
+  //   super.initState();
+  // }
+
+  //***** Another Screen shift code with Terneary Operator
+  var activeScreen = 'Start_Screen';
+
+  void switchScreen() {
     setState(() {
-      activeScreen = QuestionScreen();
+      activeScreen = 'Question_Screen';
     });
   }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    // final screenWidget = activeScreen == 'Start_Screen'
+    //     ? Start_Screen(switchScreen)
+    //     : const QuestionScreen();
+
+    // other type to decelear this statement
+    Widget screenWidget = Start_Screen(switchScreen);
+    if(activeScreen == 'Question_Screen')
+      {
+        screenWidget = const QuestionScreen();
+      }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
-            decoration:const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color.fromARGB(255, 0, 205, 253),
-                    Color.fromARGB(255, 156, 0, 208)],
-                  begin: Alignment.topCenter,
-                  end: Alignment.topLeft,
-                )
-            ),
-            child: activeScreen,
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 0, 205, 253),
+              Color.fromARGB(255, 156, 0, 208)
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.topLeft,
+          )),
+          // child: activeScreen,
+          child: screenWidget,
         ),
       ),
     );
