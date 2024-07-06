8471d9d55ba5cbdb5d1555b358393f643d8bd8c7
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-07-06 07:38:45 +0600

    Update question_screen.dart

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
-class QuestionScreen extends StatefulWidget
-{
+class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
+
   State<QuestionScreen> createState() {
     return _questionScreenState();
   }
 }
-class _questionScreenState extends State<QuestionScreen>
-{
+
+class _questionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[0];
     // TODO: implement build
-    return Text ("Question Scrren");
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+           Text(currentQuestion.text, style: TextStyle(
+            color: Colors.white,
+          ),),
+          const SizedBox(height: 30,),
+          Answer_Button(currentQuestion.answer[0], (){}),
+          const SizedBox(height: 30,),
+          Answer_Button(currentQuestion.answer[1],(){}),
+          const SizedBox(height: 30,),
+          Answer_Button(currentQuestion.answer[2],(){}),
+          const SizedBox(height: 30,),
+          Answer_Button(currentQuestion.answer[3],(){}),
+        ],),
+    );
   }
 }
