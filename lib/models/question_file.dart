60d8fb63939f8e6ff47930fa61f98fa4cd218321
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-07-07 11:46:53 +0600

    Update question_file.dart

--- lib/models/question_file.dart
+++ lib/models/question_file.dart
@@ -3,4 +3,12 @@ class QuizQuestion {
   final String text;
   final List<String> answer;
 
+  List<String>getshuffleAnswer()
+  {
+    final shuffledlist = List.of(answer);
+  shuffledlist.shuffle();
+    return shuffledlist;
+
+  }
+
 }
