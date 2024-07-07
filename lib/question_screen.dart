37fffc31e7c61260fa55710d3c39fca40a4c78f7
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-07-07 11:46:56 +0600

    Update question_screen.dart

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -18,21 +18,40 @@ class _questionScreenState extends State<QuestionScreen> {
     // TODO: implement build
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-           Text(currentQuestion.text, style: TextStyle(
-            color: Colors.white,
-          ),),
-          const SizedBox(height: 30,),
-          Answer_Button(currentQuestion.answer[0], (){}),
-          const SizedBox(height: 30,),
-          Answer_Button(currentQuestion.answer[1],(){}),
-          const SizedBox(height: 30,),
-          Answer_Button(currentQuestion.answer[2],(){}),
-          const SizedBox(height: 30,),
-          Answer_Button(currentQuestion.answer[3],(){}),
-        ],),
+      child: Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+
+             Text(currentQuestion.text, style: TextStyle(
+              color: Colors.white,
+               fontSize: 20,
+
+            ),
+               textAlign: TextAlign.center,
+             ),
+            const SizedBox(height: 30,),
+           // here we can use ... for create a list itarble
+
+            ...currentQuestion.getshuffleAnswer().map((answer){
+              return Answer_Button(answer,(){});
+            }),
+
+            // ...currentQuestion.answer.map((answer){
+           //    return Answer_Button(answer,(){});
+           //  }),
+
+            // Answer_Button(currentQuestion.answer[0], (){}),
+            // const SizedBox(height: 30,),
+            // Answer_Button(currentQuestion.answer[1],(){}),
+            // const SizedBox(height: 30,),
+            // Answer_Button(currentQuestion.answer[2],(){}),
+            // const SizedBox(height: 30,),
+            // Answer_Button(currentQuestion.answer[3],(){}),
+          ],),
+      ),
     );
   }
 }
