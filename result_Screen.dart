80d4abee69dd5bd9e4b11fc938570f75c64823fa
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-07-12 21:21:33 +0600

    Update my app result scrren'

---
+++ result_Screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class result_scrren extends StatelessWidget {
+  const result_scrren({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
