fe044224483584dbf29f9fed85499ace2caefa40
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-07-14 23:55:52 +0600

    Fianl my quiz app

--- lib/Question_Summary.dart
+++ lib/Question_Summary.dart
@@ -7,24 +7,28 @@ class question_summary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: summaryData.map((data){
-      return Row(children: [
-      // type custing
-        Text(((data['question_index'] as int) + 1).toString()),
-        Expanded(
-          child: Column(children: [
-            Text(data['Question']as String),
-            SizedBox(height: 5,),
-            Text(data['User_answer']as String),
-              Text(data['Corrrect_Answer']as String),
-          
-          
-            ],),
-        ),
-
-      ],);
-  }).toList(),
-  );
+    return SizedBox(height:300,
+      child: SingleChildScrollView(
+        child: Column(children: summaryData.map((data){
+          return Row(children: [
+          // type custing
+            Text(((data['question_index'] as int) + 1).toString()),
+            Expanded(
+              child: Column(children: [
+                Text(data['Question']as String),
+                SizedBox(height: 5,),
+                Text(data['User_answer']as String),
+                  Text(data['Corrrect_Answer']as String),
+        
+        
+                ],),
+            ),
+        
+          ],);
+          }).toList(),
+          ),
+      ),
+    );
 
   }
 }
